bccdbae068c681fa96e59b6416c75cf4d298b39b
donovan <donovan@donovanwalker.com>
2025-04-26 23:02:03 -0500

    event-list: Tweak css for better small page presentation & geist font.

--- src/components/EventList.tsx
+++ src/components/EventList.tsx
@@ -10,9 +10,9 @@ type EventListProps = {
     eventList: EONEvent[]
 }
 
-const eventItem: string = `grid gap-4 items-center py-3 border-b border-gray-200
+const eventItem: string = `grid gap-4 items-center py-3 border-b border-gray-200 font-[family-name:var(--font-geist-mono)]
      grid-cols-[50px_100px_130px]
-     sm:grid-cols-[50px_100px_130px_1fr] 
+     sm:grid-cols-[50px_100px_130px_1fr]
                `;
 
 export const EventList: React.FC<EventListProps> = ({eventList}) => {
@@ -28,7 +28,7 @@ export const EventList: React.FC<EventListProps> = ({eventList}) => {
                 <span className="link"><Link href={`/event/${event.id}`}><LinkIcon/></Link></span>
                 <time className="dateYMD">{event.dateYMD}</time>
                 <span className="category">{event.categoryName}</span>
-                <span className="eventTitle col-span-3 sm:col-span-1">{event.title}</span>
+                <span className="eventTitle col-span-4 sm:col-span-1">{event.title}</span>
             </li>
         ))
         }
